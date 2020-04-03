abeb72e5eb5205d9ac0f9299ae791b7bb5540dc8
Your <you@example.com>
2020-04-04 01:25:42 +0800

    up

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ cargo build --release
 cd ../docker-immt-check-balance
 cp ../immt-check-balance/target/release/immt-check-balance ./
 sudo docker build . -t ivories/balance
+
+
+sudo git commit -a -mup
+sudo git push
